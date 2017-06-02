2f98d4785cf8ada6a7b445e2679114c88c09e8cb
mdeschne <mdeschne@users.noreply.github.com>
2017-06-02 16:59:53 -0500

    mobile url fix

--- app/controllers/youtube_validator.rb
+++ app/controllers/youtube_validator.rb
@@ -2,6 +2,7 @@ class YoutubeValidator < ActiveModel::Validator
   def validate(record)
     unless record.media_url.starts_with? 'www.youtube.com'
           unless record.media_url.starts_with? 'https://www.youtube.com'
+            unless record.media_url.starts_with? 'https://m.youtube.com'
       record.errors[:media_url] << 'must be a valid Youtube address'
     end
   end
